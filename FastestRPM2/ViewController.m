6a6ff1459ee54b960f7877c4edd79565de44e4b2
Benson Huynh <quoc@shaw.ca>
2016-01-21 12:24:00 -0800

    final version with stretch

--- FastestRPM2/ViewController.m
+++ FastestRPM2/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *meterImage;
 @property (weak, nonatomic) IBOutlet UIImageView *dialIMage;
 @property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panAction;
+@property (weak, nonatomic) IBOutlet UILabel *displaySpeed;
 
 @end
 
@@ -29,25 +30,26 @@
 
 - (IBAction)panFinger:(UIPanGestureRecognizer *)sender {
     if (sender.state == UIGestureRecognizerStateChanged ) {
-    double fullCircle = 2*M_PI;
-    double startPoint = (3*M_PI_4);
-    double endRange = M_PI_4;
-    double rangeOfSpeed = 5000;
-    double speedDialRange = fullCircle - (startPoint - endRange);
-    CGPoint vector = [sender velocityInView:self.view];
-    double velocity = sqrt((vector.x * vector.x) + (vector.y * vector.y));
-    NSLog(@"Speed is %f km/hr", velocity);
-    double dialPercentage = velocity/rangeOfSpeed;
-    double actualSpeed = (speedDialRange * dialPercentage) + startPoint;
-    self.dialIMage.transform = CGAffineTransformMakeRotation(actualSpeed);
-} else if (sender.state == UIGestureRecognizerStateEnded) {
-    [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(setToStartingPoint) userInfo:nil repeats:YES];
-}
+        double fullCircle = 2*M_PI;
+        double startPoint = (3*M_PI_4);
+        double endRange = M_PI_4;
+        double rangeOfSpeed = 5000;
+        double speedDialRange = fullCircle - (startPoint - endRange);
+        CGPoint vector = [sender velocityInView:self.view];
+        double velocity = sqrt((vector.x * vector.x) + (vector.y * vector.y));
+        self.displaySpeed.text = [NSString stringWithFormat:@"%.2f km/hr", velocity];
+        NSLog(@"Speed is %f km/hr", velocity);
+        double dialPercentage = velocity/rangeOfSpeed;
+        double actualSpeed = (speedDialRange * dialPercentage) + startPoint;
+        self.dialIMage.transform = CGAffineTransformMakeRotation(actualSpeed);
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(setToStartingPoint) userInfo:nil repeats:YES];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-
+    
 }
 
 @end
